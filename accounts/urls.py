6dc3c793aa66564007f14e57a48c8537755a1a25
weshy <josephwaweru96@gmail.com>
2022-03-22 20:04:24 +0300

    login and logout requests

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('customer_register/', views.customer_register.as_view(), name='customer_register'),
     path('employee_register/', views.customer_register.as_view(), name='employee_register'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
+from django.contrib import messages
 from django.views.generic import CreateView
-from .models import User, Customer, Employee
+
 from .forms import CustomerSignUpForm, EmployeeSignUpForm
+from .models import Customer, Employee, User
 
 
 # Create your views here.
@@ -14,12 +18,38 @@ class customer_register(CreateView):
     form_class = CustomerSignUpForm
     template_name = 'customer_register.html'
 
-    # def validation(self, form):
-    #     user = form.save()
-    #     login(self.request,user)
-    #     return redirect('/')
+    def validation(self, form):
+        user = form.save()
+        login(self.request,user)
+        return redirect('/')
 
-class customer_register(CreateView):
+class employee_register(CreateView):
     model = User
-    form_class = CustomerSignUpForm
-    template_name = 'customer_register.html'
+    form_class = EmployeeSignUpForm
+    template_name = 'employee_register.html'
+
+    def validation(self, form):
+        user = form.save()
+        login(self.request,user)
+        return redirect('/')
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+                messages.error(request, "Invalid username or password")
+    return render(request, 'login.html',
+    context={'form':AuthenticationForm()})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
